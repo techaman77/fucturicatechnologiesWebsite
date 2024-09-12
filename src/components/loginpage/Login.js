12a4e6dac5945bfb6ee3e1ff1d1be22af3505a12
ManavJain-09 <manav@fucturicatechnologies.in>
2024-09-12 17:52:36 +0530

    Updated login page

--- src/components/loginpage/Login.js
+++ src/components/loginpage/Login.js
@@ -67,13 +67,17 @@ const Login = () => {
 
   return (
     <>
-      <div className="w-full flex flex-col md:flex-row sm:flex-row min-h-screen font-montserrat">
+      <div className="w-full flex flex-col md:flex-row min-h-screen font-montserrat">
         <div className="w-full md:w-1/2 h-64 md:h-screen bgimage bg-cover bg-center relative">
           <div className='logo absolute top-8 left-1/2 transform -translate-x-1/2 w-80 h-40 bg-contain bg-no-repeat'></div>
         </div>
-        <div className="flex w-full md:w-1/2 flex-col justify-center items-center p-4 md:p-8">
-          <div className='absolute md:top-44 sm:top-36 right-0 md:w-40 sm:w-10 h-10 bg-contain bg-no-repeat plane-icon'></div>
-          <h2 className="text-3xl md:text-5xl text-gradient font-semibold tracking-wide">WELCOME</h2>
+        <div className="relative flex w-full md:w-1/2 flex-col justify-center items-center p-4 md:p-8">
+  
+          <div className="flex flex-col items-end relative w-full items-center justify-center">
+            <div className='absolute right-[-30px] top-0 w-36 h-32 md:w-40 md:h-40 bg-contain bg-no-repeat plane-icon'></div>
+            <h2 className="text-3xl md:text-5xl text-gradient font-semibold tracking-wide mt-16 md:mt-24">WELCOME</h2>
+          </div>
+          
           <p className="text-gray-600 text-base md:text-lg text-center mb-4">Login with Email</p>
           <form onSubmit={handleSubmit} className="flex flex-col gap-5 w-full max-w-md mx-auto items-center">
             <input
@@ -97,8 +101,8 @@ const Login = () => {
               <p className="text-red-500 mt-2 text-center">{errorMessage}</p>
             )}
 
-            <div className="flex w-full justify-end">
-              <span className="px-3 text-gray-500 cursor-pointer">forgot password?</span>
+            <div className="flex w-full justify-end md:mr-[100px] sm:mr-[50px]">
+              <span className="px-3  text-gray-500 cursor-pointer">forgot password?</span>
             </div>
 
             <div className="flex items-center justify-center w-full mt-4">
