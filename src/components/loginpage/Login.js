5bb747a4dd6c993bd0b09d7f007adda4b3fd3cce
ManavJain-09 <manav@fucturicatechnologies.in>
2024-09-13 12:33:06 +0530

    Updated login page

--- src/components/loginpage/Login.js
+++ src/components/loginpage/Login.js
@@ -66,32 +66,54 @@ const Login = () => {
 
   return (
     <>
-      <div className="w-full flex h-[700px] font-montserrat">
-        <div className="w-[50%] h-full bgimage"></div>
-        <div className="flex w-[50%] flex-col justify-center items-center">
-          <h2 className="text-[50px] text-gradient font-semibold tracking-wide">WELCOME</h2>
-          <p className="text-[#666666] text-[18px] text-center">Login with Email</p>
-          <form onSubmit={handleSubmit} className="flex flex-col gap-5 py-10">
+      <div className="w-full flex flex-col md:flex-row sm:flex-row min-h-screen font-montserrat">
+        <div className="w-full md:w-1/2 h-screen bgimage bg-cover bg-center relative bg-no-repeat">
+          <div className='logo absolute top-8 left-1/2 transform -translate-x-1/2 w-80 h-40 bg-contain bg-no-repeat'></div>
+        </div>
+        <div className="flex w-full md:w-1/2 flex-col justify-center items-center p-4 md:p-8">
+          <div className="relative flex flex-col items-center w-full">
+            <div className="flex justify-between items-center w-full">
+              <div className="flex-grow"></div>
+                <div className='plane-icon absolute right-0 w-40 h-20 md:w-40 sm:w-30 md:h-40 bg-contain bg-no-repeat mb-2 right-[-40px]'></div>
+              </div>
+              <h2 className="text-3xl md:text-5xl text-gradient font-semibold tracking-wide">WELCOME</h2>
+              <p className="text-gray-600 text-base md:text-lg text-center mb-4">Login with Email</p>
+          </div>  
+          <form onSubmit={handleSubmit} className="flex flex-col gap-5 w-full max-w-md mx-auto items-center">
             <input
               placeholder="Email Id"
               type="email"
-              className="bg-transparent border-2 border-[#666666] rounded-lg p-2"
+              className="bg-transparent border-2 border-gray-600 rounded-lg p-2 w-full max-w-xs email-icon"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <input
               placeholder="Password"
               type="password"
-              className="bg-transparent border-2 border-[#666666] rounded-lg p-2"
+              className="bg-transparent border-2 border-gray-600 rounded-lg p-2 w-full max-w-xs password-icon"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button type="submit" className="mt-4 bg-gradient-to-r from-[#007BFF] to-[#0056B3] text-white p-2 rounded-lg" disabled={isSubmitting}>
+      
+            <button type="submit" className="mt-4 bg-gradient-to-r from-blue-500 to-blue-700 text-white p-2 rounded-lg w-full max-w-xs" disabled={isSubmitting}>
               {isSubmitting ? 'Logging in...' : 'Login'}
             </button>
+
+            <div className="relative w-full">
+              <div className="absolute right-6"> 
+                <span className="text-gray-500 cursor-pointer">forgot password?</span>
+              </div>
+            </div>
+
             {errorMessage && (
               <p className="text-red-500 mt-2">{errorMessage}</p>
-            )}
+            )} 
+
+            <div className="flex items-center justify-center w-full mt-4">
+              <hr className="w-full border-t border-gray-300" />
+              <span className="px-3 text-gray-500">OR</span>
+              <hr className="w-full border-t border-gray-300" />
+            </div>
           </form>
         </div>
       </div>
