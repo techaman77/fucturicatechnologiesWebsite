5e2ddcd891081c641038ce50ccbee39a25883ecc
sachinsingh <sachinsingh020406@gmail.com>
2024-09-17 12:53:37 +0530

    sachin17/09/24

--- src/components/Homepage/Blogs.js
+++ src/components/Homepage/Blogs.js
@@ -1,53 +1,58 @@
-import React from 'react'
-import img1 from './assets/b1.svg'
-import img2 from './assets/b2.svg'
-import img3 from './assets/b3.svg'
+import React from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
-
-const blogPosts = [
-    {
-        img: img1,
-        title: 'The Future of IT Training: Why Hands-On Experience Matters',
-        description: 'Understanding the practical applications of technology.',
-    },
-    {
-        img: img2,
-        title: 'Transforming Careers with BPOTraining',
-        description: 'Developing the skills needed to excel in the BPO industry.',
-    },
-    {
-        img: img3,
-        title: 'Embracing Data Science: A Career Path with Endless Possibilities',
-        description: 'Covering data analysis, machine learning, and more.',
-    }
-];
+import { useNavigate } from "react-router-dom";
+import { blogPosts } from "./blogPosts";
 
 const Blogs = () => {
-    return (
-        <div className='w-full flex justify-center py-20 max-sm:py-0'>
-            <div className='w-[90%]'>
-                <h2 className='text-[#666666] text-center text-[48px] max-xl:text-[35px] max-lg:text-[28px] max-sm:text-[24px] font-semibold'>
-                    Our Blog <span className='text-[#FB861E]'>Posts</span>
-                </h2>
-                <div className='text-[#666666] grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-20 max-sm:gap-10 mx-20 max-md:mx-10 max-sm:mx-0 py-10'>
-                    {blogPosts.map((post, index) => (
-                        <div key={index} className='flex flex-col gap-3'>
-                            <img src={post.img} alt='blog post' />
-                            <div className='flex items-center gap-5'>
-                                <h2 className='text-[20px] max-xl:text-[16px] font-medium'>{post.title}</h2>
-                                <MdOutlineArrowOutward className='text-[#FB861E] text-[44px] m-auto' />
-                            </div>
-                            <p className='font-normal text-[16px] max-xl:text-[14px]'>{post.description}</p>
-                            <div className='flex gap-5 max-xl:gap-2'>
-                                <button className='text-[#007BFF] max-xl:text-[14px] bg-[#007BFF] hover:text-[#fff] hover:bg-[#007BFF] bg-opacity-[0.1] rounded-[20px] py-1 px-4'>Leadership</button>
-                                <button className='text-[#666666] max-xl:text-[14px] bg-[#666666] hover:text-[#fff] hover:bg-[#666666] bg-opacity-[0.1] rounded-[20px] py-1 px-4'>Management</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Blogs
+  // Function to handle clicking on a blog post
+  const handleBlogClick = (id) => {
+    navigate(`/blog/${id}`); // Navigate to the blog detail page with the blog ID
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center py-20 max-sm:py-0">
+      <div className="w-[90%]">
+        <h2 className="text-[#666666] text-center text-[48px] max-xl:text-[35px] max-lg:text-[28px] max-sm:text-[24px] font-semibold">
+          Our Blog <span className="text-[#FB861E]">Posts</span>
+        </h2>
+
+        <div className="text-[#666666] grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-20 py-10 mx-20 max-md:mx-10 max-sm:mx-0">
+          {blogPosts.map((post, index) => (
+            <div
+              key={index}
+              className="flex flex-col gap-3 cursor-pointer "
+              onClick={() => handleBlogClick(post.id)} // Pass the blog ID
+            >
+              <img
+                src={post.img}
+                alt="blog post"
+                className="w-full h-[200px] object-cover rounded-[30px] duration-300 hover:scale-110"
+              />
+              <div className="flex items-center gap-5">
+                <h2 className="text-[20px] max-xl:text-[16px] font-medium">
+                  {post.title}
+                </h2>
+                <MdOutlineArrowOutward className="text-[#FB861E] text-[44px] m-auto" />
+              </div>
+              <p className="font-normal text-[16px] max-xl:text-[14px]">
+                {post.description}
+              </p>
+              <div className="flex gap-5 max-xl:gap-2">
+                <button className="text-[#007BFF] max-xl:text-[14px] bg-[#007BFF] hover:text-[#fff] hover:bg-[#007BFF] bg-opacity-[0.1] rounded-[20px] py-1 px-4">
+                  Leadership
+                </button>
+                <button className="text-[#666666] max-xl:text-[14px] bg-[#666666] hover:text-[#fff] hover:bg-[#666666] bg-opacity-[0.1] rounded-[20px] py-1 px-4">
+                  Management
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,85 +1,162 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { logout } from '../../store/auth';
-import logo from './assets/logo.svg';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { FaXmark } from 'react-icons/fa6';
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { logout } from "../../store/auth";
+import logo from "./assets/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaXmark } from "react-icons/fa6";
 import { IoLogOutOutline } from "react-icons/io5";
-import axios from 'axios';
-import './navbar.css';
-import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
+import axios from "axios";
+import "./navbar.css";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
-    const location = useLocation();
-    const currentPath = location.pathname;
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const username = localStorage.getItem('name');  // Assuming user is a string
+  const username = localStorage.getItem("name"); // Assuming user is a string
 
-    const handleToggleMenu = () => setOpen(!open);
+  const handleToggleMenu = () => setOpen(!open);
 
-    const userId = useSelector((state) => state.auth.userId);
+  const userId = useSelector((state) => state.auth.userId);
 
-    const handleLogout = async () => {
-        try {
-            await axios.post('https://futurica-backend.vercel.app/logout', { userId });
+  const handleLogout = async () => {
+    try {
+      await axios.post("https://futurica-backend.vercel.app/logout", {
+        userId,
+      });
 
-            dispatch(logout());
+      dispatch(logout());
 
-            navigate('/login');
-        } catch (error) {
-            console.error('Logout failed:', error);
-        }
-    };
-    return (
-        <div className='w-full flex justify-center py-5 max-sm:py-4 border-b border-[#000]'>
-            <div className='h-full w-[90%] text-[#666666] max-sm:text-[#fff] flex items-center justify-between max-sm:items-center'>
-                <Link to="/">
-                    <img className='max-lg:w-[200px] max-md:w-[180px] max-sm:w-[150px]' src={logo} alt='logo' />
-                </Link>
-                <div className={`${currentPath === '/admin-panel' || currentPath === '/employee-panel' ? 'hidden' : ''}`}>
-                    <ul className={`w-full gap-5 max-lg:gap-3 justify-end flex items-center max-sm:flex-col max-sm:justify-center max-sm:gap-10 max-sm:absolute max-sm:bg-black max-sm:bg-opacity-95 z-[5] max-sm:w-[100%] max-sm:h-screen ${open ? 'top-[0%] left-0' : 'top-[-1490px]'}`}>
-                        <Link to="/about-us" onClick={handleToggleMenu}>
-                            <li className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${currentPath === '/' ? 'active' : ''}`}>About US</li>
-                        </Link>
-                        <Link to="/services" onClick={handleToggleMenu}>
-                            <li className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${currentPath === '/services' ? 'active' : ''}`}>Services</li>
-                        </Link>
-                        <Link to="/career" onClick={handleToggleMenu}>
-                            <li className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${currentPath === '/career' ? 'active' : ''}`}>Career</li>
-                        </Link>
-                        <Link to="/training" onClick={handleToggleMenu}>
-                            <li className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${currentPath === '/courses' ? 'active' : ''}`}>Training</li>
-                        </Link>
-                        <Link to="/blogs" onClick={handleToggleMenu}>
-                            <li className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${currentPath === '/blogs' ? 'active' : ''}`}>Blogs</li>
-                        </Link>
-                        <Link to="/contact-us" onClick={handleToggleMenu}>
-                            <li className={`text-navElements font-montserrat max-lg:text-[16px] text-[#fff] bg-[#FB861E] borde rounded-[40px] py-2 max-md:text-[14px] max-sm:text-[14px] tracking-wide font-semibold px-3 text-center ${currentPath === '/contact' ? 'active' : ''}`}>Contact Us <EastOutlinedIcon /></li>
-                        </Link>
-                    </ul>
-                    <div onClick={handleToggleMenu} className='block text-[#666666] sm:hidden absolute right-[5%] top-[3%] z-[6]'>
-                        {open ? <FaXmark className='w-[30px] h-[30px]' /> : <GiHamburgerMenu className='w-[20px] h-[25px]' />}
-                    </div>
-                </div>
-                <div className={`${currentPath === '/admin-panel' || currentPath === '/employee-panel' ? 'flex' : 'hidden'} gap-5`}>
-                    <h2 className='border-2 border-opacity-[0.6] border-[#666666] rounded-[30px] p-2 px-4 text-[18px] font-bold'>{username}</h2>
-                    <Link to='/reset-password'><button className={`${currentPath === '/employee-panel' ? 'hidden' : ''} border-2 border-opacity-[0.6] border-[#666666] rounded-[30px] p-2 px-4 text-[18px] font-bold`}>Update Password</button></Link>
-                    <button
-                        onClick={handleLogout}
-                        className={`${currentPath === '/employee-panel' ? 'hidden' : ''} flex flex-col items-center text-[20px] max-xl:text-[18px] max-lg:text-[16px] font-semibold text-[#666666]`}
-                    >
-                        <IoLogOutOutline className='text-[24px] max-xl:text-[20px] max-lg:text-[18px]' />
-                        Log Out
-                    </button>
-                </div>
-            </div>
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+  return (
+    <div className="w-full flex justify-center py-5 max-sm:py-4 border-b border-[#000]">
+      <div className="h-full w-[90%] text-[#666666] max-sm:text-[#fff] flex items-center justify-between max-sm:items-center">
+        <Link to="/">
+          <img
+            className="max-lg:w-[200px] max-md:w-[180px] max-sm:w-[150px]"
+            src={logo}
+            alt="logo"
+          />
+        </Link>
+        <div
+          className={`${
+            currentPath === "/admin-panel" || currentPath === "/employee-panel"
+              ? "hidden"
+              : ""
+          }`}
+        >
+          <ul
+            className={`w-full gap-5 max-lg:gap-3 justify-end flex items-center max-sm:flex-col max-sm:justify-center max-sm:gap-10 max-sm:absolute max-sm:bg-black max-sm:bg-opacity-95 z-[5] max-sm:w-[100%] max-sm:h-screen ${
+              open ? "top-[0%] left-0" : "top-[-1490px]"
+            }`}
+          >
+            <Link to="/about-us" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${
+                  currentPath === "/" ? "active" : ""
+                }`}
+              >
+                About US
+              </li>
+            </Link>
+            <Link to="/services" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${
+                  currentPath === "/services" ? "active" : ""
+                }`}
+              >
+                Services
+              </li>
+            </Link>
+            <Link to="/career" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${
+                  currentPath === "/career" ? "active" : ""
+                }`}
+              >
+                Career
+              </li>
+            </Link>
+            <Link to="/training" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${
+                  currentPath === "/courses" ? "active" : ""
+                }`}
+              >
+                Training
+              </li>
+            </Link>
+            <Link to="/blogs" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements text-gray font-montserrat max-lg:text-[16px] max-md:text-[12px] max-sm:text-[14px] tracking-wide font-semibold text-center hover max-sm:mb-5 ${
+                  currentPath === "/blogs" ? "active" : ""
+                }`}
+              >
+                Blogs
+              </li>
+            </Link>
+            <Link to="/contact-us" onClick={handleToggleMenu}>
+              <li
+                className={`text-navElements font-montserrat max-lg:text-[16px] text-[#fff] bg-[#FB861E] borde rounded-[40px] py-2 max-md:text-[14px] max-sm:text-[14px] tracking-wide font-semibold px-3 text-center ${
+                  currentPath === "/contact" ? "active" : ""
+                }`}
+              >
+                Contact Us <EastOutlinedIcon />
+              </li>
+            </Link>
+          </ul>
+          <div
+            onClick={handleToggleMenu}
+            className="block text-[#666666] sm:hidden absolute right-[5%] top-[3%] z-[6]"
+          >
+            {open ? (
+              <FaXmark className="w-[30px] h-[30px]" />
+            ) : (
+              <GiHamburgerMenu className="w-[20px] h-[25px]" />
+            )}
+          </div>
         </div>
-    );
+        <div
+          className={`${
+            currentPath === "/admin-panel" || currentPath === "/employee-panel"
+              ? "flex"
+              : "hidden"
+          } gap-5`}
+        >
+          <h2 className="border-2 border-opacity-[0.6] border-[#666666] rounded-[30px] p-2 px-4 text-[18px] font-bold">
+            {username}
+          </h2>
+          <Link to="/reset-password">
+            <button
+              className={`${
+                currentPath === "/employee-panel" ? "hidden" : ""
+              } border-2 border-opacity-[0.6] border-[#666666] rounded-[30px] p-2 px-4 text-[18px] font-bold`}
+            >
+              Update Password
+            </button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className={`${
+              currentPath === "/employee-panel" ? "hidden" : ""
+            } flex flex-col items-center text-[20px] max-xl:text-[18px] max-lg:text-[16px] font-semibold text-[#666666]`}
+          >
+            <IoLogOutOutline className="text-[24px] max-xl:text-[20px] max-lg:text-[18px]" />
+            Log Out
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
